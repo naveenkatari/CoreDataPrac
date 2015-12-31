637fe6b2ab9db287804d026a5a9322d71de422db
Naveen Katari <naveen.katari@sourcebits.com>
2015-12-31 16:07:04 +0530

    Initial Commit

---
+++ CoreDataPrac/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreDataPrac
+//
+//  Created by Naveen Katari on 31/12/15.
+//  Copyright © 2015 Sourcebits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
